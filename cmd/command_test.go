5cf5229757b41b656764bfae487abdf82ee4f841
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-08-22 13:21:51 +0700

    test: Add test for insert

--- cmd/command_test.go
+++ cmd/command_test.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/jeremylombogia/cli-tasks-efishery/internal"
+	"github.com/rs/xid"
+
 	"github.com/jeremylombogia/cli-tasks-efishery/configs"
 	"github.com/stretchr/testify/assert"
 )
@@ -13,12 +16,30 @@ var client, db = configs.Init()
 
 // TestInsert ...
 func TestInsert(t *testing.T) {
-	assert.Equal(t, true, true, "Insert with affected insert not equal")
+	guid := xid.New()
+
+	var createTask = internal.Task{
+		ID:      guid.String(),
+		Content: "Lorem ipsum dolor si amet",
+		IsDone:  false,
+		Tag:     "College",
+	}
+
+	var store, _ = internal.Store(&createTask)
+
+	var showTask internal.Task
+
+	var foundTask = internal.Show(store.ID)
+	if err := foundTask.ScanDoc(&showTask); err != nil {
+		panic(err)
+	}
+
+	// TODO:: Add all data except rev
+	assert.Equal(t, store.ID, showTask.ID, "Insert with affected insert not equal")
 }
 
 func TestPing(t *testing.T) {
 	var testPing, _ = client.Ping(context.TODO())
 
 	assert.Equal(t, testPing, true)
-
 }
