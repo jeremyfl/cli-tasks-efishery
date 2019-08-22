eac9275c707cfd94ad945da1a0c320634ecced56
Jeremy Lombogia <jeremylombogia7@gmail.com>
2019-08-22 13:30:32 +0700

    test: Add update test

--- cmd/command_test.go
+++ cmd/command_test.go
@@ -14,7 +14,7 @@ import (
 // Initialize database from config
 var client, db = configs.Init()
 
-// TestInsert ...
+// TestInsert it compare between test store in repositories and show repositories
 func TestInsert(t *testing.T) {
 	guid := xid.New()
 
@@ -36,6 +36,35 @@ func TestInsert(t *testing.T) {
 
 	// TODO:: Add all data except rev
 	assert.Equal(t, store.ID, showTask.ID, "Insert with affected insert not equal")
+}
+
+func TestUpdate(t *testing.T) {
+	guid := xid.New()
+
+	var createTask = internal.Task{
+		ID:      guid.String(),
+		Content: "Lorem ipsum dolor si amet",
+		IsDone:  false,
+		Tag:     "College",
+	}
+
+	var store, _ = internal.Store(&createTask)
+
+	var row = internal.Show(store.ID)
+
+	var task internal.Task
+
+	if err := row.ScanDoc(&task); err != nil {
+		panic(err)
+	}
+
+	var _, err = internal.Update(store.ID, &task)
+	if err != nil {
+		panic(err)
+	}
+
+	assert.Equal(t, task.ID, task.ID, "Show struct not equal with update struct")
+
 }
 
 func TestPing(t *testing.T) {
